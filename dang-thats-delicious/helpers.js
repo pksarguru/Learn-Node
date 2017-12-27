35c4d520d40b672b12d8ffeb069a78ccfcb101ef
Pavan Sarguru <pksarguru@users.noreply.github.com>
2017-12-27 11:03:44 -0600

    through setup

--- dang-thats-delicious/helpers.js
+++ dang-thats-delicious/helpers.js
@@ -20,6 +20,8 @@ exports.icon = (name) => fs.readFileSync(`./public/images/icons/${name}.svg`);
 // Some details about the site
 exports.siteName = `Now That's Delicious!`;
 
+exports.title = `Hello`;
+
 exports.menu = [
   { slug: '/stores', title: 'Stores', icon: 'store', },
   { slug: '/tags', title: 'Tags', icon: 'tag', },
