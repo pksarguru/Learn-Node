ae2b0c457b52377a1c6919c6feec5d56395e9951
Pavan Sarguru <pksarguru@users.noreply.github.com>
2017-12-27 13:53:57 -0600

    create edit route and form

--- dang-thats-delicious/controllers/storeController.js
+++ dang-thats-delicious/controllers/storeController.js
@@ -29,5 +29,16 @@ exports.getStores = async (req, res) => {
 };
 
 exports.editStore = async (req, res) => {
-  
+  const store = await Store.findOne({ _id: req.params.id });
+  res.render('editStore', { title: `Edit ${store.name}`, store })
+
+};
+
+exports.updateStore = async (req, res) => {
+  const store = await Store.findOneAndUpdate({ _id: req.params.id}, req.body, {
+    new: true,
+    runValidators: true
+  }).exec();
+  req.flash('success', `Successfully updated ${store.name} <a href="/stores/${store.slug}"> View Store</a>`);
+  res.redirect(`/stores/${store._id}/edit`);
 };
