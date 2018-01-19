d9c21cd873e205c9714e1983642bebc3ab1bc913
Pavan Sarguru <pksarguru@users.noreply.github.com>
2018-01-19 15:05:05 -0600

    add initial password reset flow

---
+++ dang-thats-delicious/handlers/mail.js
@@ -0,0 +1,26 @@
+const nodemailer = require('nodemailer');
+const pug = require('pug');
+const juice = require('juice');
+const htmlToText = require('html-to-text');
+const promisify = require('es6-promisify');
+
+const transport = nodemailer.createTransport({
+  host: process.env.MAIL_HOST,
+  port: process.env.MAIL_PORT,
+  auth: {
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS
+  }
+});
+
+exports.send = async (options) => {
+  const mailOptions = {
+    from: `Pavan <pavan@example.com>`,
+    to: options.user.email,
+    subject: options.subject,
+    html: 'This will be filled in later.',
+    text: 'This will also be filled in later'
+  };
+  const sendMail = promisify(transport.sendMail, transport);
+  return sendMail(mailOptions);
+}
